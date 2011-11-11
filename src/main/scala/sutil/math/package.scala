79a3f54e6a510d119a8c3a31bd414156dc171588
Pablo Lalloni <plalloni@gmail.com>
2011-11-11 18:57:50 -0300

    Add sequence math operation

--- src/main/scala/sutil/math/package.scala
+++ src/main/scala/sutil/math/package.scala
@@ -1,9 +1,13 @@
 package sutil.math
+import scala.annotation.tailrec
 
 trait MathImports {
 
   def powers(base: BigInt, from: Int = 0, step: Int = 1): Stream[BigInt] =
     Stream.cons(base.pow(from), powers(base, from + step, step))
+
+  def sequence(from: BigInt, step: BigInt = 1): Stream[BigInt] =
+    Stream.cons(from, sequence(from + step, step))
 
 }
 
