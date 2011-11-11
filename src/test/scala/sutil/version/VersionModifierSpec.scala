341cdcb0ba813799ebd08f0f307751c544371580
Pablo Lalloni <plalloni@gmail.com>
2011-11-11 11:13:45 -0300

    Improve version modifier ordering spec

--- src/test/scala/sutil/version/VersionModifierSpec.scala
+++ src/test/scala/sutil/version/VersionModifierSpec.scala
@@ -11,7 +11,7 @@ class VersionModifierSpec extends Spec with ShouldMatchers {
   describe("A VersionModifier") {
 
     it("should sort appropriately") {
-      val mods = Seq[VersionModifier]("Final", "ALPHA3", "snapshot", "cr1", "beta", "beta2")
+      val mods = Seq[VersionModifier]("Final", "ALPHA3", "sp2", "snapshot", "cr1", "beta", "beta2")
       println(mods)
       println(mods.sorted)
       mods.sorted should be (Seq(M("snapshot"), M("ALPHA", V(3)), M("beta"), M("beta", V(2)), M("cr", V(1)), M("Final")))
